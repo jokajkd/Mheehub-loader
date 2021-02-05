a3d89bcf26cb1248c089134da2a4414ed89b2b59
jokajkd <oliwier549@gmail.com>
2021-02-05 22:21:05 +0100

    Add files via upload

---
+++ MheeHubLoader.lua
@@ -0,0 +1,8 @@
+nouni=false
+local function lo(l)
+loadstring(game:HttpGet(l,true))()
+nouni=true
+end
+if game.PlaceId==2753915549 then
+lo("https://raw.githubusercontent.com/BunnySalf/Hentai/main/MHEEHUB")
+end
